c29972bc9c197a33f23bbd9eb20ed26478cf14d3
youngseo9603 <youngseo9603@gmail.com>
2023-03-02 17:18:47 +0900

    [level 2] Title: 예상 대진표, Time: 0.02 ms, Memory: 10 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12985. 예상 대진표/예상 대진표.py
@@ -0,0 +1,12 @@
+import math
+def solution(n,a,b):
+    answer = 0
+
+    while(a!=b):
+        a = math.ceil(a/2)
+        b = math.ceil(b/2)
+        answer += 1
+        print(a,b)
+    
+
+    return answer
