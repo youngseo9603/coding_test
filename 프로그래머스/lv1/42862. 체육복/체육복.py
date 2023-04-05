7f1d02962d50e977d8f08e3f6279ee51ccda5712
youngseo9603 <youngseo9603@gmail.com>
2023-04-05 22:59:40 +0900

    [level 1] Title: 체육복, Time: 0.01 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/42862. 체육복/체육복.py
@@ -0,0 +1,24 @@
+def solution(n, lost, reserve):
+    answer = 0
+    
+    clothes = [1 for i in range(n+2)]
+    for i in lost:
+        clothes[i] -= 1
+    for i in reserve:
+        clothes[i] += 1
+    
+    
+    for i in range(1, len(clothes)):
+        if clothes[i] == 0 and clothes[i-1] == 2:
+            clothes[i] += 1
+            clothes[i-1] -= 1
+        elif clothes[i] == 0 and clothes[i+1] == 2:
+            clothes[i] += 1
+            clothes[i+1] -= 1
+    
+    for i in clothes:
+        if i == 0:
+            answer += 1
+    answer = n - answer
+    
+    return answer
