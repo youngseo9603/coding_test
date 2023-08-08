f9f4701209d3cb774c4626a05c62c010236ae51a
youngseo9603 <youngseo9603@gmail.com>
2023-08-08 16:42:20 +0900

    [Silver IV] Title: 빙고, Time: 120 ms, Memory: 14068 KB -BaekjoonHub

--- 백준/Silver/2578. 빙고/빙고.java
+++ 백준/Silver/2578. 빙고/빙고.java
@@ -17,6 +17,7 @@ public class Main {
 		}
 		
 		int cnt = 0;
+		int find = 0;
 		for(int i=0; i<5; i++) {
 			st = new StringTokenizer(bf.readLine());
 			for(int j=0; j<5; j++) {
@@ -38,27 +39,27 @@ public class Main {
 				
 				arr[x][y] = 0;
 				
-				int find = 0;
-				for(int a=0; a<5; a++) {
-					if(arr[a][0] == 0 && arr[a][1] == 0 && arr[a][2] == 0 && arr[a][3] == 0 && arr[a][4] == 0 )
-						find++;
-					if(arr[0][a] == 0 && arr[1][a] == 0 && arr[2][a] == 0 && arr[3][a] == 0 && arr[4][a] == 0 )
-						find++;
-				}
-				if(arr[0][0] == 0 && arr[1][1] == 0 && arr[2][2] == 0 && arr[3][3] == 0 && arr[4][4] == 0 )
-					find++;
-				if(arr[4][0] == 0 && arr[3][1] == 0 && arr[2][2] == 0 && arr[1][3] == 0 && arr[0][4] == 0 )
-					find++;
+//				int find = 0;
+//				for(int a=0; a<5; a++) {
+//					if(arr[a][0] == 0 && arr[a][1] == 0 && arr[a][2] == 0 && arr[a][3] == 0 && arr[a][4] == 0 )
+//						find++;
+//					if(arr[0][a] == 0 && arr[1][a] == 0 && arr[2][a] == 0 && arr[3][a] == 0 && arr[4][a] == 0 )
+//						find++;
+//				}
+//				if(arr[0][0] == 0 && arr[1][1] == 0 && arr[2][2] == 0 && arr[3][3] == 0 && arr[4][4] == 0 )
+//					find++;
+//				if(arr[4][0] == 0 && arr[3][1] == 0 && arr[2][2] == 0 && arr[1][3] == 0 && arr[0][4] == 0 )
+//					find++;
 				
 				
-//				if(arr[x][0] == 0 && arr[x][1] == 0 && arr[x][2] == 0 && arr[x][3] == 0 && arr[x][4] == 0 )
-//					find++;
-//				if(arr[0][y] == 0 && arr[1][y] == 0 && arr[2][y] == 0 && arr[3][y] == 0 && arr[4][y] == 0 )
-//					find++;
-//				if(x==y && arr[0][0] == 0 && arr[1][1] == 0 && arr[2][2] == 0 && arr[3][3] == 0 && arr[4][4] == 0 )
-//					find++;
-//				if(x+y==4 && arr[4][0] == 0 && arr[3][1] == 0 && arr[2][2] == 0 && arr[1][3] == 0 && arr[0][4] == 0 )
-//					find++;
+				if(arr[x][0] == 0 && arr[x][1] == 0 && arr[x][2] == 0 && arr[x][3] == 0 && arr[x][4] == 0 )
+					find++;
+				if(arr[0][y] == 0 && arr[1][y] == 0 && arr[2][y] == 0 && arr[3][y] == 0 && arr[4][y] == 0 )
+					find++;
+				if(x==y && arr[0][0] == 0 && arr[1][1] == 0 && arr[2][2] == 0 && arr[3][3] == 0 && arr[4][4] == 0 )
+					find++;
+				if(x+y==4 && arr[4][0] == 0 && arr[3][1] == 0 && arr[2][2] == 0 && arr[1][3] == 0 && arr[0][4] == 0 )
+					find++;
 				
 				if(find >= 3) {
 					System.out.println(cnt);
