0a56f6171764b37b50a08bb5f485e1c0d76c4d86
youngseo9603 <youngseo9603@gmail.com>
2024-01-17 11:10:23 +0900

    [level 2] Title: 도넛과 막대 그래프, Time: 613.81 ms, Memory: 507 MB -BaekjoonHub

--- 프로그래머스/2/258711. 도넛과 막대 그래프/도넛과 막대 그래프.java
+++ 프로그래머스/2/258711. 도넛과 막대 그래프/도넛과 막대 그래프.java
@@ -2,33 +2,99 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[][] edges) {
-        Map<Integer, int[]> map = new HashMap<>();
         int[] answer = new int[4];
-        for (int[] edge : edges) {
-            int[] value = map.getOrDefault(edge[0], new int[2]);
-            value[0]++;
-            map.put(edge[0], value);
-
-            value = map.getOrDefault(edge[1], new int[2]);
-            value[1]++;
-            map.put(edge[1], value);
-
+        
+        HashMap<Integer, List<Integer>> to = new HashMap<>();
+        HashMap<Integer, List<Integer>> from = new HashMap<>();
+        
+        List<Integer> list;
+        int max_node = 0;
+        for(int i=0; i<edges.length; i++){
+            list = new ArrayList<>();
+            list = to.getOrDefault(edges[i][0], list);
+            list.add(edges[i][1]);
+            to.put(edges[i][0], list);
+            
+            list = new ArrayList<>();
+            list = from.getOrDefault(edges[i][1], list);
+            list.add(edges[i][0]);
+            from.put(edges[i][1], list);
+            
+            max_node = Math.max(edges[i][0] , max_node);
+            max_node = Math.max(edges[i][1] , max_node);
         }
-        for (int i = 1; i <= map.size(); i++) {
-            int[] value = map.get(i);
-            if (value[0] >= 2) {
-                if (value[1] == 0) {
-                    answer[0] = i;
-                }
-                if (value[1] >= 2) {
-                    answer[3]++;
-                }
-            }
-            if (value[0] == 0 && value[1] > 0) {
-                answer[2]++;
+        
+        int cnt = 0;
+        for(int i : to.keySet()){
+            
+            if(!from.containsKey(i) && to.get(i).size() > cnt){
+                answer[0] = i;
+                cnt = to.get(i).size();
             }
         }
-        answer[1] = map.get(answer[0])[0] - answer[2] - answer[3];
+        
+        List<Integer> start = to.get(answer[0]);
+        boolean[] visited;
+        for(int s : start){
+            visited = new boolean[max_node+1];
+            Stack<Integer> stack = new Stack<>();
+            stack.add(s);
+            visited[s] = true;
+            boolean circle = false;
+            int max_cnt = 0;
+            int time = 0;
+            while(!stack.isEmpty()){
+                int temp = stack.pop();
+
+                cnt = 0;
+                if(to.containsKey(temp)){
+                    for(int t : to.get(temp))
+                        if(t!=answer[0])
+                            cnt++;
+                }
+                if(from.containsKey(temp)){
+                    for(int t : from.get(temp))
+                        if(t!=answer[0])
+                            cnt++;
+                }
+                max_cnt = Math.max(max_cnt, cnt);
+                
+                // if(temp == s && time == 0){
+                //     if(to.containsKey(s) && to.get(s).size()==1 && to.get(s).get(0)==s){
+                //         max_cnt = 2;
+                //     }
+                // }
+                
+                
+                
+                if(to.containsKey(temp)){
+                    List<Integer> to_list = to.get(temp);
+                    for(int t : to_list){
+                        if(t == s){
+                            circle = true;
+                            continue;
+                        }
+                        else if (visited[t])
+                            continue;
+                        stack.add(t);
+                        visited[t] = true;
+                    }
+                }
+                time++;
+            }
+            
+            if(max_cnt == 4)
+                answer[3]++;
+            else{
+                if(circle)
+                    answer[1]++;
+                else
+                    answer[2]++;
+            }
+                
+            
+        }
+        
         return answer;
     }
 }
